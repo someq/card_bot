37f12e22d3a1c4cb559e70435a969297593611ee
farid <farid@retechlabs.com>
2023-11-20 16:34:00 +0600

    Управление картами

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import os
 import traceback as tb
 import random
 import json
-from datetime import datetime, timedelta
 from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, CallbackQueryHandler, MessageHandler, filters
 
@@ -21,27 +20,24 @@ if TOKEN is None:
     raise RuntimeError('Telegram token is not set.')
 
 
-with open('data.json') as f:
-    # create data if it does not exist
-    data = json.load(f)
+actions = {}
 
 
-# {users login + last time}
-#  actions progress
-actions = {
-    # 'username': {
-    #     'login': 'datetime',
-    #     'new_image': {
-    #         'url': ...,
-    #         'text': ...
-    #     },
-    #     'new_user': {
-    #         'username': ...,
-    #         'password': ...,
-    #         'password_confirm': ...,
-    #     }
-    # }
-}
+if os.path.exists('data.json'):
+    with open('data.json') as f:
+        data = json.load(f)
+else:
+    data = {
+        'users': [],
+        'images': []
+    }
+    with open('data.json', 'w') as f:
+        json.dump(data, f)
+
+
+def save_data():
+    with open('data.json', 'w') as f:
+        json.dump(data, f)
 
 
 def error_wrap(func, exc_types=None):
@@ -53,33 +49,47 @@ def error_wrap(func, exc_types=None):
             await func(update, context)
         except exc_types as e:
             error = tb.format_exc()
-            await context.bot.send_message(chat_id=update.effective_chat.id, text=error)
             logger.error(error)
+            if DEBUG:
+                await context.bot.send_message(chat_id=update.effective_chat.id, text=error)
+            else:
+                await context.bot.send_message(chat_id=update.effective_chat.id, text=f'Ошибка: {e}')
+
+    return wrapper
+
+
+def admin_wrap(func):
+    async def wrapper(update: Update, context: ContextTypes.DEFAULT_TYPE):
+        username = update.effective_user.username
+        if username in data['users']:
+            await func(update, context)
+        else:
+            await unknown(update, context)
 
     return wrapper
 
 
 _user_menu = InlineKeyboardMarkup([
     [
-        InlineKeyboardButton('Получить изображение', callback_data='_get_image'),
+        InlineKeyboardButton('Вытянуть карту', callback_data='_get_card'),
     ],
 ])
 
 
 _admin_menu = InlineKeyboardMarkup([
     [
-        InlineKeyboardButton('Список изображений', callback_data='_list_images'),
-        InlineKeyboardButton('Добавить изображение', callback_data='_add_image'),
-        InlineKeyboardButton('Удалить изображение', callback_data='_delete_image'),
+        InlineKeyboardButton('Вся колода', callback_data='_list_cards'),
+        InlineKeyboardButton('Добавить карту', callback_data='_add_card_init'),
+        InlineKeyboardButton('Убрать карту', callback_data='_remove_card_init'),
     ],
     [
-        InlineKeyboardButton('Список админов', callback_data='_list_admins'),
-        InlineKeyboardButton('Добавить админа', callback_data='_add_admin'),
-        InlineKeyboardButton('Удалить админа', callback_data='_delete_admin'),
+        InlineKeyboardButton('Админы', callback_data='_list_admins'),
+        InlineKeyboardButton('Добавить админа', callback_data='_add_admin_init'),
+        InlineKeyboardButton('Удалить админа', callback_data='_delete_admin_init'),
     ],
     [
-        InlineKeyboardButton('Скачать данные', callback_data='_export_data'),
-        InlineKeyboardButton('Загрузить данные', callback_data='_import_data'),
+        InlineKeyboardButton('Скачать данные', callback_data='_save_data'),
+        InlineKeyboardButton('Загрузить данные', callback_data='_load_data_init'),
     ],
 ])
 
@@ -88,26 +98,19 @@ _admin_menu = InlineKeyboardMarkup([
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text="Привет, я тестовый бот для отправки изображений с подписями",
+        text="Привет, позолоти ручку?!",
         reply_markup=_user_menu,
     )
 
 
 @error_wrap
+@admin_wrap
 async def admin(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    users = [user['username'] for user in data['users']]
-    if update.effective_user.username in users:
-        # check last users login time.
-        # if more than ..., ask password
-        # if password is correct - update last login and allow menu
-        # for all actions check login time and does not allow if is not logged.
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text=f'Привет, {update.effective_user.username}',
-            reply_markup=_admin_menu,
-        )
-    else:
-        await unknown(update, context)
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=f'Привет, {update.effective_user.username}',
+        reply_markup=_admin_menu,
+    )
 
 
 @error_wrap
@@ -118,10 +121,118 @@ async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
 
 
+@admin_wrap
 async def _get_image(update: Update, context: ContextTypes.DEFAULT_TYPE):
     image = random.choice(data['images'])
     await context.bot.send_message(chat_id=update.effective_chat.id, text=image['url'])
     await context.bot.send_message(chat_id=update.effective_chat.id, text=image['text'])
+
+
+@admin_wrap
+async def _list_cards(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if len(data['images']) > 0:
+        message = '\n'.join(f"{i + 1}. {image['url']} {image['text']}" for i, image in enumerate(data['images']))
+    else:
+        message = 'Колода пуста'
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=message, disable_web_page_preview=True)
+
+
+@admin_wrap
+async def _add_card_init(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text='Введите ссылку на изображение и текст через пробел, например:\n'
+             'https://example.com/image.jpg Моя красивая карта',
+        disable_web_page_preview=True,
+    )
+    actions[f'{update.effective_user.username}_{update.effective_chat.id}'] = '_add_card_complete'
+
+
+@admin_wrap
+async def _add_card_complete(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    parts = update.message.text.split(' ', maxsplit=1)
+    url = parts[0].strip()
+    text = parts[1].strip() if len(parts) > 1 else ""
+    data['images'].append({'url': url, 'text': text})
+    save_data()
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=f'Новая карта:\nСсылка: {url}\nТекст: {text}',
+    )
+
+
+@admin_wrap
+async def _remove_card_init(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text='Введите порядковый номер карты в списке:',
+    )
+    actions[f'{update.effective_user.username}_{update.effective_chat.id}'] = '_remove_card_complete'
+
+
+@admin_wrap
+async def _remove_card_complete(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    try:
+        id = int(update.message.text.strip())
+        card = data['images'].pop(id - 1)
+    except ValueError:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text='Неправильный номер')
+    except IndexError:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text='Номера нет в списке')
+    else:
+        save_data()
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=f'Удалена карта:\nСсылка: {card["url"]}\nТекст: {card["text"]}',
+        )
+
+@admin_wrap
+async def _list_admins(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    pass
+
+
+@admin_wrap
+async def _add_admin_init(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    pass
+
+
+@admin_wrap
+async def _admin_admin_complete(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    pass
+
+
+@admin_wrap
+async def _remove_admin_init(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    pass
+
+
+@admin_wrap
+async def _remove_admin_complete(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    pass
+
+
+@admin_wrap
+async def _save_data(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    pass
+
+
+@admin_wrap
+async def _load_data_init(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    pass
+
+
+@admin_wrap
+async def _load_data_complete(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    pass
+
+
+@error_wrap
+async def action(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    action_name = actions.pop(f'{update.effective_user.username}_{update.effective_chat.id}', None)
+    if action_name is not None:
+        handler = globals().get(action_name)
+        if handler is not None:
+            await handler(update, context)
 
 
 @error_wrap
@@ -129,7 +240,6 @@ async def menu(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
     await query.answer()
 
-    print(query.data)
     handler = globals().get(query.data)
     if handler is not None:
         await handler(update, context)
@@ -147,6 +257,9 @@ if __name__ == '__main__':
     menu_handler = CallbackQueryHandler(menu)
     application.add_handler(menu_handler)
 
+    action_handler = MessageHandler(~filters.COMMAND, action)
+    application.add_handler(action_handler)
+
     unknown_handler = MessageHandler(filters.COMMAND, unknown)
     application.add_handler(unknown_handler)
 
@@ -158,3 +271,6 @@ if __name__ == '__main__':
 
 # https:/-/amvera.ru/?utm_source=habr&utm_medium=article&utm_campaign=oblako_dlya_botov#rec626926404
 # possible inline.
+# possible login with password
+# possible actions with multi-message dialog
+# possible ids for images
